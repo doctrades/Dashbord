c051c0e8ffac8dc4930c829454d293409873d6a2
doctrades <trade.namanrajsingh@gmail.com>
2026-04-27 18:51:02 +0530

    Add files via upload

--- live_accounts_tradeData.js
+++ live_accounts_tradeData.js
@@ -10,6 +10,14 @@ const tradeData = [
       { date: '2026-5-2', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
       { date: '2026-5-3', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
       { date: '2026-5-4', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
-      { date: '2026-5-5', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' }
+      { date: '2026-5-5', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
+      { date: '2026-5-6', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
+      { date: '2026-5-5', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
+      { date: '2026-5-5', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
+      { date: '2026-5-5', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
+      { date: '2026-5-5', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
+      { date: '2026-5-5', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
+      { date: '2026-5-5', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
+      { date: '2026-5-5', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
       // Add entries above this line
     ];
