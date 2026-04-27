89013b279b82d982e4d1c021c224161bf6d22758
doctrades <trade.namanrajsingh@gmail.com>
2026-04-27 22:19:55 +0530

    Update live_accounts_tradeData.js

--- live_accounts_tradeData.js
+++ live_accounts_tradeData.js
@@ -3,21 +3,10 @@
     // ──────────────────────────────────────────────
 
 const startBalance = 30;
-const target = 26022;
+const target = 100;
 
-var daysLeft = 100;
+var daysCompleted = 0;
 const tradeData = [
       { date: '2026-5-2', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
-      { date: '2026-5-3', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
-      { date: '2026-5-4', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
-      { date: '2026-5-5', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
-      { date: '2026-5-6', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
-      { date: '2026-5-5', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
-      { date: '2026-5-5', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
-      { date: '2026-5-5', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
-      { date: '2026-5-5', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
-      { date: '2026-5-5', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
-      { date: '2026-5-5', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
-      { date: '2026-5-5', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
       // Add entries above this line
     ];
