c9719115f73fb4e7b49285e29f46523e77c990f4
doctrades <trade.namanrajsingh@gmail.com>
2026-04-29 13:49:50 +0530

    Update live_accounts_tradeData.js

--- live_accounts_tradeData.js
+++ live_accounts_tradeData.js
@@ -7,6 +7,6 @@ const target = 100;
 
 var daysCompleted = 0;
 const tradeData = [
-      { date: '2026-5-2', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
+      //{ date: '2026-5-2', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
       // Add entries above this line
     ];
