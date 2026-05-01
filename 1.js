1d1f3f117588b1549f5f83ebed2f4d670d3325f9
doctrades <trade.namanrajsingh@gmail.com>
2026-05-01 16:48:44 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -7,12 +7,7 @@ const target = 100;
 
 var daysCompleted = 0;
 const tradeData = [
-    { date: '2026-5-2', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
-    { date: '2026-5-2', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
-    { date: '2026-5-2', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
-    { date: '2026-5-2', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
-    { date: '2026-5-2', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
-    { date: '2026-5-2', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
-    { date: '2026-5-2', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
+    //{ date: '2026-5-2', pair: 'XAUUSD', trades: 3, pnl: 2.14, pct: 7.13, note: 'Success' },
+
     //   Add entries above this line
 ];
