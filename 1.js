ed367c74e2a4d75703bc5cc45a298e094ff521a1
doctrades <trade.namanrajsingh@gmail.com>
2026-05-04 22:32:19 +0530

    Add files via upload

--- 1.js
+++ 1.js
@@ -7,7 +7,7 @@ const target = 100;
 
 var daysCompleted = 0;
 const tradeData = [
-    { date: '2026-5-1', pair: 'XAUUSD', trades: 3, pnl: -5.3, pct: 16.7, note: 'Failed' },
+    //{ date: '2026-5-1', pair: 'XAUUSD', trades: 3, pnl: -5.3, pct: 16.7, note: 'Failed' },
 
     //   Add entries above this line
 ];
